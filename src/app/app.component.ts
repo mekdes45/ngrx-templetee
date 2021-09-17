600d360d566fafeba594fd458c352b96bb5e2771
mekdes45 <mekmis2020@gmail.com>
2021-09-17 07:56:03 -0700

    fix paths in appModule.ts and store/index.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './store';
+import { loginAuth } from './store/actions/auth/auth.actions';
+import { loggedInUserSelector } from './store/selectors/auth/auth.selectors';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mean-tutorial';
+  loggedInUser$: Observable<User | null>;
+  constructor(private store: Store<AppState>) {
+    this.store.dispatch(loginAuth({
+      data: { username: 'Mesi', password: 'Password' }
+      
+    }));
+    this.loggedInUser$=this.store.select(loggedInUserSelector)
+  }
 }
